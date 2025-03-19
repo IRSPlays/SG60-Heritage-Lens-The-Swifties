0043a78865c081725142fd74dc92dac2e614112e
IRSPlays <haziqshahzai2021@hotmail.com>
2025-03-19 16:14:00 +0800

    HELLO

---
+++ SG60 Heritage Lens The Swifties/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  SG60 Heritage Lens The Swifties
+//
+//  Created by Apple on 19/3/25.
+//
+
+import Foundation
+import Supabase
+
+let Supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://ctzxhracmnuzetlwfqbd.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0enhocmFjbW51emV0bHdmcWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzNjM3ODQsImV4cCI6MjA1NzkzOTc4NH0.HuFPWTBw495Ja-iREz4pW62S4jjId1aAgInwxTClZCg"
+)
