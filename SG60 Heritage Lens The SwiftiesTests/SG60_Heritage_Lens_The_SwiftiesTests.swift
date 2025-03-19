d8312a9166fe050bce48dfc1f167186869e07799
IRSPlays <haziqshahzai2021@hotmail.com>
2025-03-19 11:17:44 +0800

    Initial Commit

---
+++ SG60 Heritage Lens The SwiftiesTests/SG60_Heritage_Lens_The_SwiftiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SG60_Heritage_Lens_The_SwiftiesTests.swift
+//  SG60 Heritage Lens The SwiftiesTests
+//
+//  Created by Apple on 19/3/25.
+//
+
+import Testing
+@testable import SG60_Heritage_Lens_The_Swifties
+
+struct SG60_Heritage_Lens_The_SwiftiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
