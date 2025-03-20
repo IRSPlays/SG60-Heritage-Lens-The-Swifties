62c762f74d89db902be5ed0512a56bdbd44b9693
IRSPlays <haziqshahzai2021@hotmail.com>
2025-03-20 12:01:49 +0800

    gg

---
+++ SG60 Heritage Lens The Swifties/CoremlView.swift
@@ -0,0 +1,18 @@
+//
+//  CoremlView.swift
+//  SG60 Heritage Lens The Swifties
+//
+//  Created by Apple on 20/3/25.
+//
+
+import SwiftUI
+
+struct CoremlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoremlView()
+}
